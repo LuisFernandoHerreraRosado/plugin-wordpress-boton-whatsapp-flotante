1eebdf969cb0d510373b29d2856469d739920c1e
Luis Herrera Rosado <herrerarosadoluis@gmail.com>
2026-03-11 12:22:36 -0500

    arreglo salto de linea

--- whatsapp-floating-button/whatsapp-floating-button.php
+++ whatsapp-floating-button/whatsapp-floating-button.php
@@ -423,7 +423,7 @@ class WhatsApp_Floating_Button {
                 if ( ! empty( $encoded_message ) ) {
                     $encoded_message .= "%0A%0A";
                 }
-                $encoded_message .= rawurlencode( "(Enviado desde: " . $current_url . ")" );
+                $encoded_message .= rawurlencode( "(\n Enviado desde:" . $current_url . ")" );
             }
         }
 
